b3faae169e8d90e289ca5ffe4f6d4e1bb0cab1f6
Digvijay Anney <Justypedigvijay@gmail.com>
2022-09-17 16:51:03 +0530

    BaseProject: Updated form method
    test_123

--- src/com/git/learning/packages/loginPage.java
+++ src/com/git/learning/packages/loginPage.java
@@ -4,19 +4,17 @@ import java.util.HashMap;
 
 public class loginPage {
 
-	
-	public static void enterLoginDetails(String userName,String passWord) {
-		
+	public static void enterLoginDetails(String userName, String passWord) {
+
 		System.out.println(userName);
 		System.out.println(passWord);
 	}
-	
-	public HashMap<String,String> enterFormDetails(HashMap<String,String> data) {
-		
-		HashMap<String,String> inputData = new HashMap<String,String>();
+
+	public HashMap<String, String> enterFormDetails(HashMap<String, String> data) {
+
+		HashMap<String, String> inputData = new HashMap<String, String>();
 		data.put("name", "test");
-		
-		
+		data.put("eid", "123");
 		return inputData;
 	}
 }
