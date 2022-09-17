e00348e18fc579c33ec34eddf312c47275d29678
Digvijay Anney <Justypedigvijay@gmail.com>
2022-09-17 16:22:02 +0530

    BaseProject: Added LoginPage Class Details
    test_123

---
+++ src/com/git/learning/packages/loginPage.java
@@ -0,0 +1,11 @@
+package com.git.learning.packages;
+
+public class loginPage {
+
+	
+	public static void enterLoginDetails(String userName,String passWord) {
+		
+		System.out.println(userName);
+		System.out.println(passWord);
+	}
+}
