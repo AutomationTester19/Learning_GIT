45e653d8a1cbf34ab2426cefabb241e8f4edda17
Digvijay Anney <Justypedigvijay@gmail.com>
2022-09-17 18:00:55 +0530

    BaseProject: Added register contact function in RegistrationPage Class
    test_123

--- src/com/git/learning/packages/registrationPage.java
+++ src/com/git/learning/packages/registrationPage.java
@@ -1,9 +1,21 @@
 package com.git.learning.packages;
 
+import java.util.HashMap;
+
 public class registrationPage {
 
 	
-	public void registerContact() {
+	public HashMap<String, String> registerContact() {
 		
+		String contact_id=" ";
+		String contact_Name=" ";
+		String ph_num=" ";
+		
+		HashMap<String,String> contactMap = new HashMap<String,String>();
+		contactMap.put("contactId", contact_id);
+		contactMap.put("contactName", contact_Name);
+		contactMap.put("ph_Num", ph_num);
+
+		return contactMap;	
 	}
 }
